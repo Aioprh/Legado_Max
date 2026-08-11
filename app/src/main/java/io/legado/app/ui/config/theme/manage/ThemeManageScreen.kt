1777b7b6f84d95c3c32885a1e96039ba15d6a445
xingzheyousan <youfeng.knight@outlook.com>
2026-08-11 12:10:57 +0800

    fix(theme)：修复引用

--- app/src/main/java/io/legado/app/ui/config/theme/manage/ThemeManageScreen.kt
+++ app/src/main/java/io/legado/app/ui/config/theme/manage/ThemeManageScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
