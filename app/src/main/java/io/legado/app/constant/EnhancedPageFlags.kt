29869dc7598f19a5dd3b5dd1df7aa298675daed3
Aioprh <108421380+Aioprh@users.noreply.github.com>
2026-09-09 23:02:45 +0800

    chore: remove redundant enhanced page flag constants

--- app/src/main/java/io/legado/app/constant/EnhancedPageFlags.kt
+++
@@ -1,7 +0,0 @@
-package io.legado.app.constant
-
-/** Optional page enhancements. All flags are opt-in and default to false. */
-object EnhancedPageFlags {
-    const val ENHANCED_EXPLORE_PAGE = "enhanced_explore_page"
-    const val ENHANCED_RSS_PAGE = "enhanced_rss_page"
-}
