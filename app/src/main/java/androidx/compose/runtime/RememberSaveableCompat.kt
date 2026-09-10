7f45ca17e91c3da3a649925bfa9626fea677003d
Aioprh <108421380+Aioprh@users.noreply.github.com>
2026-09-10 11:16:28 +0800

    fix: 修复 rememberSaveable 泛型约束

--- app/src/main/java/androidx/compose/runtime/RememberSaveableCompat.kt
+++ app/src/main/java/androidx/compose/runtime/RememberSaveableCompat.kt
@@ -7,4 +7,4 @@ import androidx.compose.runtime.saveable.rememberSaveable as rememberSaveableDel
  * Compose 的实际实现位于 runtime.saveable，这里仅做转发。
  */
 @Composable
-fun <T> rememberSaveable(init: () -> T): T = rememberSaveableDelegate { init() }
+fun <T : Any> rememberSaveable(init: () -> T): T = rememberSaveableDelegate { init() }
